258beb15618f4669946c1f89c39fe7a2d70160f9
galaxyfreak <marinspajic17@hotmail.com>
2014-12-30 10:27:55 +0100

    Android.mk: Symlink modem firmware
    Change-Id: I24c2534fe5e91bca9a387432d4a4e64f5343201c

--- Android.mk
+++ Android.mk
@@ -1,4 +1,43 @@
 #
-# This empty Android.mk file exists to prevent the build system from
-# automatically including any other Android.mk files under this directory.
+# Copyright (C) 2014 The Android Open-Source Project
 #
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+LOCAL_PATH := $(call my-dir)
+
+ifeq ($(TARGET_DEVICE),seagull)
+include $(call all-subdir-makefiles,$(LOCAL_PATH))
+
+# Create firmware links
+FIRMWARE_IMAGES := \
+	adsp.b00 adsp.b01 adsp.b02 adsp.b03 adsp.b04 adsp.b05 adsp.b06 \
+	adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.mdt \
+	modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
+	modem.b08 modem.b10 modem.mdt modem.b11 modem.b13 modem.b14 \
+	modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 \
+	modem.b21 modem.b22 modem.b25 modem.b26 modem.b27 \
+	wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
+	wcnss.b08 wcnss.b09 wcnss.mdt \
+	mba.b00 mba.mdt
+
+FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
+$(FIRMWARE_SYMLINKS): $(LOCAL_INSTALLED_MODULE)
+	@echo "Firmware link: $@"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	$(hide) ln -sf /firmware/image/$(notdir $@) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(FIRMWARE_SYMLINKS)
+
+endif

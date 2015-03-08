aaf2c6912fe6e66cf97fee7bbab731407bf2b166
Adam Farden <adam@farden.cz>
2015-03-08 19:01:31 +0100

    seagull: fix up missing firmware symlinks
    These links are perfectly fine, however RIL is broken,
    we probably need some extra edits in libril.
    
    Change-Id: I185c1fd457c5772e6af7f63f9125b687a8a48e7f
    Signed-off-by: Adam Farden <adam@farden.cz>

--- Android.mk
+++ Android.mk
@@ -24,11 +24,11 @@ FIRMWARE_IMAGES := \
 	adsp.b00 adsp.b01 adsp.b02 adsp.b03 adsp.b04 adsp.b05 adsp.b06 \
 	adsp.b07 adsp.b08 adsp.b09 adsp.b10 adsp.b11 adsp.b12 adsp.mdt \
 	modem.b00 modem.b01 modem.b02 modem.b03 modem.b04 modem.b05 \
-	modem.b08 modem.b10 modem.mdt modem.b11 modem.b13 modem.b14 \
-	modem.b15 modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 \
-	modem.b21 modem.b22 modem.b25 modem.b26 modem.b27 \
+	modem.b08 modem.b10 modem.b11 modem.b13 modem.b14 modem.b15 \
+	modem.b16 modem.b17 modem.b18 modem.b19 modem.b20 modem.b21 \
+	modem.b22 modem.b25 modem.b26 modem.b27 modem.mdt \
 	wcnss.b00 wcnss.b01 wcnss.b02 wcnss.b04 wcnss.b06 \
-	wcnss.b08 wcnss.b09 wcnss.mdt \
+	wcnss.b07 wcnss.b08 wcnss.b09 wcnss.mdt \
 	mba.b00 mba.mdt
 
 FIRMWARE_SYMLINKS := $(addprefix $(TARGET_OUT_ETC)/firmware/,$(notdir $(FIRMWARE_IMAGES)))
